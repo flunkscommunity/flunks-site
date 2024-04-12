60d144da5d2f829cad78139dc19be7c720369e47
Ervin Mamutov <employervin@gmail.com>
2024-04-12 20:38:30 +0200

    update nextjs version

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,22 +1,18 @@
 import { createGlobalStyle } from "styled-components";
 import { styleReset } from "react95";
-// @ts-ignore
-import ms_sans_serif from "react95/dist/fonts/ms_sans_serif.woff2";
-// @ts-ignore
-import ms_sans_serif_bold from "react95/dist/fonts/ms_sans_serif_bold.woff2";
 import React from "react";
 
 export const GlobalStyles = React.memo(createGlobalStyle`
   ${styleReset}
   @font-face {
     font-family: 'ms_sans_serif';
-    src: url('${ms_sans_serif}') format('woff2');
+    src: url('/fonts/ms_sans_serif.woff2') format('woff2');
     font-weight: 400;
     font-style: normal
   }
   @font-face {
     font-family: 'ms_sans_serif';
-    src: url('${ms_sans_serif_bold}') format('woff2');
+    src: url('/fonts/ms_sans_serif_bold.woff2') format('woff2');
     font-weight: bold;
     font-style: normal
   }
