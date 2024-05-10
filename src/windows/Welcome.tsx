15e2ac973c37a2e6b3fee7d068428e7263b98f8f
Gerald Regan <hakametal@gmail.com>
2024-05-10 17:50:02 +0200

    Add anchors to the tips

--- src/windows/Welcome.tsx
+++ src/windows/Welcome.tsx
@@ -5,9 +5,10 @@ import { useWindowsContext } from "contexts/WindowsContext";
 import { WINDOW_IDS } from "fixed";
 import { Button, Frame, Checkbox } from "react95";
 import styled from "styled-components";
+import GumballMachine from "./GumballMachine";
 
 const Welcome: React.FC = () => {
-  const { closeWindow } = useWindowsContext();
+  const { closeWindow, openWindow } = useWindowsContext();
   const [tipIndex, setTipIndex] = useState(0);
 
   const handleNextTip = () => {
@@ -15,46 +16,105 @@ const Welcome: React.FC = () => {
   };
 
   const tips = [
-    "[1/5] Flunks are cute but mischievous high-schoolers wreaking havoc on Flow",
-    "[2/5] Flunks95 is where you'll be able to interact with anything Flunks",
-    "[3/5] You'll need to create an account and login with a non-custodial wallet like Lilico and Blocto to have full access to Flunks95",
-    "[4/5] You can get Flunks items on marketplaces like Flowty",
-    "[5/5] You can stake you Flunks items at the Gumball Machine to earn $GUM",
+    <p>
+      [1/5] Flunks are cute but mischievous high-schoolers wreaking havoc on
+      Flow
+    </p>,
+    <p>
+      [2/5] Flunks95 is where you'll be able to interact with anything Flunks
+    </p>,
+    <p>
+      [3/5] You'll need to create an account and login with a non-custodial
+      wallet like{" "}
+      <a
+        href="https://lilico.app/"
+        target="_blank"
+        className="underline text-blue-500"
+      >
+        Lilico
+      </a>{" "}
+      and{" "}
+      <a
+        href="https://blocto.io/"
+        target="_blank"
+        className="underline text-blue-500"
+      >
+        Blocto
+      </a>{" "}
+      to have full access to Flunks95
+    </p>,
+    <p>
+      [4/5] You can get Flunks items on marketplaces like{" "}
+      <a
+        target="_blank"
+        href="https://www.flowty.io/collection/0x807c3d470888cc48/Flunks"
+        className="underline text-blue-500"
+      >
+        Flowty
+      </a>
+    </p>,
+    <p>
+      [5/5] You can stake you Flunks items at the{" "}
+      <span
+        onClick={() =>
+          openWindow({
+            key: WINDOW_IDS.GUMBALL_MACHINE,
+            window: <GumballMachine />,
+          })
+        }
+        className="underline text-blue-500 cursor-pointer"
+      >
+        Gumball Machine
+      </span>{" "}
+      to earn $GUM
+    </p>,
   ];
 
   return (
     <DraggableResizeableWindow
       offSetHeight={44}
       headerTitle="Welcome"
+      initialHeight="60%"
+      initialWidth="55%"
       authGuard={false}
       windowsId={WINDOW_IDS.WELCOME}
       onClose={() => {
         closeWindow(WINDOW_IDS.WELCOME);
       }}
     >
-      <h1 className="text-4xl">Welcome to Flunks95</h1>
-      <div className="flex">
-        <section className="basis-2/3 flex flex-col">
-          <Frame
-            className="!gap-4"
-            variant="field"
-            style={{
-              marginTop: "2rem",
-              padding: "2rem",
-              height: 200,
-              width: 500,
-            }}
-          >
-            <h2 className="text-xl font-bold">Did you know...</h2>
-            <p>{tips[tipIndex]}</p>
-          </Frame>
-          <Checkbox label="Show this Welcome Screen the next time you start Flunks95" />
-        </section>
-        <section className="basis-1/3 flex flex-col mt-8 gap-2">
-          <Button>What's New</Button>
-          <Button onClick={handleNextTip}>Next Tip</Button>
-          <Button onClick={() => closeWindow(WINDOW_IDS.WELCOME)}>Close</Button>
-        </section>
+      <div className="p-4">
+        <h1 className="text-4xl">Welcome to Flunks95</h1>
+        <div className="flex">
+          <section className="basis-2/3 flex flex-col">
+            <Frame
+              className="!gap-4"
+              variant="field"
+              style={{
+                marginTop: "2rem",
+                padding: "2rem",
+                height: 200,
+                width: 500,
+              }}
+            >
+              <h2 className="text-xl font-bold">Did you know...</h2>
+              {tips[tipIndex]}
+            </Frame>
+            <Checkbox
+              label="Show this Welcome Screen the next time you start Flunks95"
+              className="pt-2"
+            />
+          </section>
+          <section className="basis-1/3 flex flex-col mt-8 gap-2">
+            <Button>What's New</Button>
+            <Button onClick={handleNextTip}>Next Tip</Button>
+            <Button
+              onClick={() => closeWindow(WINDOW_IDS.WELCOME)}
+              className=""
+            >
+              Close
+            </Button>
+          </section>
+        </div>
       </div>
     </DraggableResizeableWindow>
   );
