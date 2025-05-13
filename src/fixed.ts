1907f303dca76d784e5006cdbfac1092714fe124
flunkscommunity <flunkscommunity@gmail.com>
2025-05-13 10:13:46 -0500

    trial1
    trial1

--- src/fixed.ts
+++ src/fixed.ts
@@ -12,7 +12,7 @@ export const WINDOW_IDS = {
   ABOUT_US: "about-us",
   FLUNKFOLIO_ITEM: "flunkfolio-item-",
   FLUNK_E_MART: "flunk-e-mart",
-  FLUNKS_HUB: "FLUNKS_HUB",
+  FLUNKSHUB: "FlunksHub",
 };
 
 export const WINDOW_APP_INFO_TO_WINDOW_ID = {
