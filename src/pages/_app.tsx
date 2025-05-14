7f9ae45f8a4074a4e71b1a4c79840984341ddc11
flunkscommunity <flunkscommunity@gmail.com>
2025-05-14 16:55:41 -0500

    updated err'thang
    updated logs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -34,7 +34,8 @@ const MyApp: AppType = ({ Component, pageProps }) => {
             <DynamicContextProvider
               settings={{
                 environmentId: "379fb92a-c707-4bcb-bf51-37d9f64ff415",
-                walletConnectors: [FlowWalletConnectors.lilico],
+                walletConnectors: FlowWalletConnectors,
+                walletsFilter: (wallet) => wallet.name === "Lilico",
               }}
             >
               <PaginatedItemsProvider>
