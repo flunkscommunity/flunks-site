6112881dad905ee3e3b2b3def5c76f55aa5f7e6c
flunkscommunity <flunkscommunity@gmail.com>
2025-05-14 15:12:15 -0500

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -37,7 +37,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
               settings={{
                 environmentId: "379fb92a-c707-4bcb-bf51-37d9f64ff415",
                 walletConnectors: [FlowWalletConnectors],
-                walletsFilter: ["Lilico"],
+                walletsFilter: (wallet) => wallet.name === "Lilico",
               }}
             >
               <PaginatedItemsProvider>
