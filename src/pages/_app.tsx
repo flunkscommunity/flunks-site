a98e9219fcca532616b093082aaa9ac5c2622642
flunkscommunity <flunkscommunity@gmail.com>
2025-05-14 16:20:57 -0500

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,7 @@ import {
   DynamicContextProvider,
   DynamicUserProfile,
 } from "@dynamic-labs/sdk-react-core";
-import { FlowWalletConnectors } from "@dynamic-labs/flow";
+import { lilico } from "@dynamic-labs/flow";
 import useThemeSettings from "store/useThemeSettings";
 import React from "react";
 import { Analytics } from "@vercel/analytics/react";
@@ -38,7 +38,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
             <DynamicContextProvider
               settings={{
                 environmentId: "379fb92a-c707-4bcb-bf51-37d9f64ff415",
-                walletConnectors: lilicoOnly,
+                walletConnectors: [lilico],
               }}
             >
               <PaginatedItemsProvider>
