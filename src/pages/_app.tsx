e3d205769025bc641bde7b21fd819347898191d7
flunkscommunity <flunkscommunity@gmail.com>
2025-05-14 14:34:02 -0500

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -33,11 +33,14 @@ const MyApp: AppType = ({ Component, pageProps }) => {
       <ThemeWrapper>
         <WindowsProvider>
           <ClaimBackpackProvider>
-            <DynamicContextProvider
+            <<DynamicContextProvider
               settings={{
               environmentId: "379fb92a-c707-4bcb-bf51-37d9f64ff415",
-              walletConnectors: FlowWalletConnectors.filter(
-                (wallet) => wallet?.name === "Lilico"
+              walletConnectors: [FlowWalletConnectors],
+              walletsFilter: (wallet) => wallet.name === "Lilico",
+  }}
+>
+
     ),
   }}
 >
