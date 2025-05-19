f2ed502ec4320b014a8480f4790d35eb42c97f66
flunkscommunity <flunkscommunity@gmail.com>
2025-05-19 17:04:25 -0500

    Update Homebase.tsx
    controller!

--- src/windows/Homebase.tsx
+++ src/windows/Homebase.tsx
@@ -42,28 +42,4 @@ const Homebase = () => {
   );
 };
 
-
-
-const Homebase = () => {
-  const { closeWindow } = useWindowsContext();
-
-  return (
-    <DraggableResizeableWindow
-      onClose={() => closeWindow(WINDOW_IDS.HOMEBASE)}
-      headerTitle="Homebase"
-      initialHeight="80%"
-      initialWidth="80%"
-      windowsId={WINDOW_IDS.HOMEBASE}
-      resizable
-      showMaximizeButton
-      headerIcon="/images/icons/backpack.png"
-    >
-      <div className="p-4 text-lg">
-        <p>🏠 Welcome to Homebase.</p>
-        <p>This is your command center for all things Flunks.</p>
-      </div>
-    </DraggableResizeableWindow>
-  );
-};
-
 export default Homebase;
