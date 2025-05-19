472d59d5db19731670baedefd0ea328d1f1c36d6
flunkscommunity <flunkscommunity@gmail.com>
2025-05-19 12:26:14 -0500

    Update homebase.tsx

--- src/windows/homebase.tsx
+++ src/windows/homebase.tsx
@@ -1,24 +1,24 @@
-// windows/LockerRoom.tsx
+// windows/Homebase.tsx
 import { Frame } from "react95";
 import { WINDOW_IDS } from "fixed";
 import { useWindowsContext } from "contexts/WindowsContext";
 import DraggableResizeableWindow from "components/DraggableResizeableWindow";
 
-const LockerRoom = () => {
+const Homebase = () => {
   const { closeWindow } = useWindowsContext();
 
   return (
     <DraggableResizeableWindow
-      onClose={() => closeWindow(WINDOW_IDS.LOCKER_ROOM)}
-      headerTitle="Locker Room"
-      windowsId={WINDOW_IDS.LOCKER_ROOM}
-      headerIcon="/images/icons/lockerroom.png"
+      onClose={() => closeWindow(WINDOW_IDS.HOMEBASE)}
+      headerTitle="Homebase"
+      windowsId={WINDOW_IDS.HOMEBASE}
+      headerIcon="/images/icons/Homebase.png"
     >
       <Frame variant="well" className="p-4">
-        <p>Welcome to the locker room. Look around for clues!</p>
+        <p>Welcome to the Homebase!</p>
       </Frame>
     </DraggableResizeableWindow>
   );
 };
 
-export default LockerRoom;
+export default Homebase;
