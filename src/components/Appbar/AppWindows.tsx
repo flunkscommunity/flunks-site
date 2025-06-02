81ded83a6c145bfc2fec1f353672b82da30d79ff
flunkscommunity <flunkscommunity@gmail.com>
2025-06-02 16:27:13 -0500

    Update AppWindows.tsx

--- src/components/Appbar/AppWindows.tsx
+++ src/components/Appbar/AppWindows.tsx
@@ -2,19 +2,31 @@ import { useWindowsContext } from "contexts/WindowsContext";
 import { Button } from "react95";
 
 const AppWindows = () => {
-  const { windowApps, bringWindowToFront, activeWindow } = useWindowsContext();
+  const {
+    windowApps,
+    bringWindowToFront,
+    restoreWindow,
+    activeWindow,
+  } = useWindowsContext();
 
   return (
     <div className="gap-2 h-full ml-2 hidden lg:flex">
       {windowApps.map((app) => {
+        const isActive = activeWindow === app.key;
+        const isMinimized = app.isMinimized;
+
         return (
           <Button
             key={app.key}
-            onClick={() => bringWindowToFront(app.key)}
-            active={activeWindow === app.key}
-            className="!flex  gap-2 max-w-[100px] flex-shrink"
+            onClick={() =>
+              isMinimized ? restoreWindow(app.key) : bringWindowToFront(app.key)
+            }
+            active={isActive && !isMinimized}
+            className="!flex gap-2 max-w-[100px] flex-shrink"
           >
-            <img src={app.appIcon} className="h-6 w-6 flex-shrink-0" />
+            {app.appIcon && (
+              <img src={app.appIcon} className="h-6 w-6 flex-shrink-0" />
+            )}
             <span className="flex-shrink max-w-full truncate hidden lg:block">
               {app.appName}
             </span>
