d7eb95e799c1d698881ac6bb5218e4afbd3846bd
flunkscommunity <flunkscommunity@gmail.com>
2025-06-16 20:58:45 -0700

    Add BootScreen component with retro boot sequence

---
+++ src/components/DesktopMain.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DesktopMain: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center w-screen h-screen bg-gray-800 text-white">
+      Desktop Main
+    </div>
+  );
+};
+
+export default DesktopMain;
