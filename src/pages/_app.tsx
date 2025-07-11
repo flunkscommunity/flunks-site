33109d305003de8e3d73d9dcc5685185c3225655
flunkscommunity <flunkscommunity@gmail.com>
2025-07-11 07:35:14 -0500

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -34,7 +34,12 @@ const MyApp: AppType = ({ Component, pageProps }) => {
             <DynamicContextProvider
               settings={{
                 environmentId: "379fb92a-c707-4bcb-bf51-37d9f64ff415",
-                walletConnectors: FlowWalletConnectors, // Use the imported connectors
+                walletConnectors: [
+                  {
+                    key: "flowwallet",
+                    name: "Flow Wallet",
+                  },
+                ],
               }}
             >
               <PaginatedItemsProvider>
